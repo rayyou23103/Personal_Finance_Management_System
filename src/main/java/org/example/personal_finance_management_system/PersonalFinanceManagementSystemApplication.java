9b76a5e20a3e7a82bf3fe7917975e0d9c579b0d1
rayyou23103 <k9102188@gmail.com>
2025-06-02 20:50:37 +0800

    建立 spring boot專案

---
+++ src/main/java/org/example/personal_finance_management_system/PersonalFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.personal_finance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/personal_finance_management_system/PersonalFinanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.personal_finance_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
